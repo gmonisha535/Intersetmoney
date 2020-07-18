8c718074db7c95c6d020dc593f0a76e5c840bb2f
gmonisha535 <61582373+gmonisha535@users.noreply.github.com>
2020-07-18 11:14:57 +0530

    Add files via upload

---
+++ week4/src/main/java/epam/com/week4/Compound.java
@@ -0,0 +1,16 @@
+package epam.com.week4;
+public class Compound {
+	double p;
+	double t;
+	double r;
+	public Compound(double p,double t,double r) {
+		this.p=p;
+		this.t=t;
+		this.r=r;
+	}
+	public double cal() {
+		return Math.round(this.p*Math.pow(1+this.r/100.0, this.t));
+	}
+}
+
+
